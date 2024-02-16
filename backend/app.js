51bfbcfa0944516a6321b629496eeeeded0096c8
Fanny Holmström <fannyholmstrom@icloud.com>
2024-02-16 13:54:47 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
+const connection = require("./lib/conn");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.get("/notes", (req, res) => {
+
+    connection.connect((err) => {
+        if (err) console.log("err", err);
+
+        let query = "SELECT * FROM todo WHERE done=0";
+
+        connection.query(query, (err, data) => {
+            if (err) console.log("err", err);
+
+            console.log("notes", data);
+            res.json(data);
+        })
+
+    })
+})
+
+module.exports = app;
